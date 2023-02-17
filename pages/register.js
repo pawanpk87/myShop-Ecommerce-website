536ebcd7e6903a519f1f9a3c282dc35966cb92f0
Pawan Kumar Mehta <arowpk@gmail.com>
2023-02-17 22:08:25 +0530

    Created Registration Functionality

--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,14 @@
 import Layout from "@/components/Layout";
-import Link from "next/link";
+import { getError } from "@/utils/error";
+import axios from "axios";
 import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { getError } from "@/utils/error";
 import { toast } from "react-toastify";
-import { useRouter } from "next/router";
 
-export default function Login() {
+export default function Register() {
   const { data: session } = useSession();
   const router = useRouter();
   const { redirect } = router.query;
@@ -22,11 +23,19 @@ export default function Login() {
     register,
     handleSubmit,
     watch,
+    getValues,
     formState: { errors },
   } = useForm();
 
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post("/api/auth/signup", {
+        redirect: false,
+        name,
+        email,
+        password,
+      });
+
       const result = await signIn("credentials", {
         redirect: false,
         email,
@@ -42,12 +51,27 @@ export default function Login() {
   };
 
   return (
-    <Layout title="Login">
+    <Layout title="Create Account">
       <form
         onSubmit={handleSubmit(submitHandler)}
         className="mx-auto max-w-screen-md"
       >
-        <h1 className="mb-4 text-xl">Login</h1>
+        <h1 className="mb-4 text-xl">Create Account</h1>
+        <div className="mb-4">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            {...register("name", {
+              required: "Please enter name",
+            })}
+            className="w-full"
+            id="name"
+            autoFocus
+          ></input>
+          {errors.name && (
+            <div className="text-red-500">{errors.name.message}</div>
+          )}
+        </div>
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
@@ -62,7 +86,6 @@ export default function Login() {
             })}
             className="w-full"
             id="email"
-            autoFocus
           ></input>
           {errors.email && (
             <div className="text-red-500">{errors.email.message}</div>
@@ -87,11 +110,36 @@ export default function Login() {
           )}
         </div>
         <div className="mb-4">
-          <button className="primary-button">Login</button>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            {...register("confirmPassword", {
+              required: "Please enter confirmPassword",
+              validate: (value) => value === getValues("password"),
+              minLength: {
+                value: 6,
+                message: "confirm password is more than 5 chars",
+              },
+            })}
+            className="w-full"
+            id="confirmPassword"
+          ></input>
+
+          {errors.confirmPassword && (
+            <div className="text-red-500">{errors.confirmPassword.message}</div>
+          )}
+
+          {errors.confirmPassword &&
+            errors.confirmPassword.type === "validate" && (
+              <div className="text-red-500">Password do not match</div>
+            )}
         </div>
         <div className="mb-4">
-          Don&apos;t have an account? &nbsp;
-          <Link href={`/register?redirect=${redirect || "/"}`}>Register</Link>
+          <button className="primary-button">Register</button>
+        </div>
+        <div className="mb-4">
+          Already have an account? &nbsp;
+          <Link href={`/login?redirect=${redirect || "/"}`}>Login</Link>
         </div>
       </form>
     </Layout>
