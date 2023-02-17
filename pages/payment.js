6586d6b4239af338d15eb155aef402a876c3f6b9
Pawan Kumar Mehta <arowpk@gmail.com>
2023-02-17 12:13:15 +0530

    Created Payment Mehtod Page

---
+++ pages/payment.js
@@ -0,0 +1,78 @@
+import CheckoutWizard from "@/components/CheckoutWizard";
+import Layout from "@/components/Layout";
+import { Store } from "@/utils/Store";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export default function Payment() {
+  const router = useRouter();
+
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("");
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress, paymentMethod } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      router.push("/shipping");
+    }
+    setSelectedPaymentMethod(paymentMethod || "");
+  }, [paymentMethod, router, shippingAddress.address]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!selectedPaymentMethod) {
+      return toast.error("Payment method is required");
+    }
+
+    dispatch({ type: "SAVE_PAYMENT_METHOD", payload: selectedPaymentMethod });
+
+    Cookies.set(
+      "car",
+      JSON.stringify({
+        ...cart,
+        paymentMethod: selectedPaymentMethod,
+      })
+    );
+
+    router.push("/placeholder");
+  };
+
+  return (
+    <Layout title="Payment Method  ">
+      <CheckoutWizard activeStep={2} />
+      <form onSubmit={submitHandler} className="mx-auto max-w-screen-md">
+        <h1 className="mb-4 text-xl">Payment Method</h1>
+        {["PayPal", "Stripe", "CashOnDelivery"].map((payment) => (
+          <div key={payment} className="mb-4">
+            <input
+              name="paymentMethod"
+              className="p-2 outline-none focus:ring-0"
+              id={payment}
+              type="radio"
+              checked={selectedPaymentMethod === payment}
+              onChange={() => setSelectedPaymentMethod(payment)}
+            />
+
+            <label className="p-2" htmlFor={payment}>
+              {payment}
+            </label>
+          </div>
+        ))}
+        <div className="mb-4 flex justify-between">
+          <button
+            onClick={() => router.push("/shipping")}
+            type="button"
+            className="default-button"
+          >
+            Back
+          </button>
+          <button className="primary-button">Next</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
--- utils/Store.js
+++ utils/Store.js
@@ -7,7 +7,7 @@ export const Store = createContext();
 const initialStore = {
   cart: Cookies.get("cart")
     ? JSON.parse(Cookies.get("cart"))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: "" },
 };
 
 function reducer(state, action) {
@@ -57,6 +57,15 @@ function reducer(state, action) {
             ...state.cart.shippingAddress,
             ...action.payload,
           },
+        },
+      };
+    }
+    case "SAVE_PAYMENT_METHOD": {
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     }
